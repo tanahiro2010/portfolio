e9f53ad1dd73e95a4a873f9a1c33fcaac8757f71
tanahiro2010 <herentongkegu087@gmail.com>
2026-02-12 20:13:53 +0900

    feat: Headerコンポーネントにスタイルを追加し、デスクトップ表示を制御するShowコンポーネントを統合

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,12 +1,20 @@
 // import { useEffect, useState } from "react";
+import { Show } from "../responsive/show";
+
 
 const Header = () => {
 
     return (
-        <header>
-            
+        <header className="m-5 p-3 rounded-lg border border-gray-200 mb-5 flex flex-col sm:flex-row items-center justify-between">
+            <h1 className="text-2xl font-bold">
+                <a href="/" className="p-0">tanahiro2010</a>
+            </h1>
+
+            <Show show="desktop" className="text-gray-600">
+                Scenario Writer / Software Developer
+            </Show>
         </header>
-    )
+    );
 }
 
 export default Header;
