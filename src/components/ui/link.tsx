43a5812b867bc5dfdbd2077c4be023a9546404ac
tanahiro2010 <herentongkegu087@gmail.com>
2026-02-12 20:13:25 +0900

    feat: LinkコンポーネントにclassNameプロパティを追加し、スタイルのカスタマイズを可能に

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -1,6 +1,13 @@
-const Link = ({ href, children }: { href: string; children: React.ReactNode }) => {
+import { cn } from "@/lib/utils";
+
+type LinkProps = {
+    href: string;
+    children: React.ReactNode;
+    className?: string;
+}
+const Link = ({ href, children, className = "" }: LinkProps) => {
     return (
-        <a href={href} className="b py-2 px-4 inline-flex items-center justify-center">
+        <a href={href} className={`b ${cn("py-2 px-4 inline-flex items-center justify-center", className)}`}>
             {children}
         </a>
     );
