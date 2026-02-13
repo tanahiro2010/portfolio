dc2a28867e6cf5949b27dfca91eedf27e90067e0
tanahiro2010 <herentongkegu087@gmail.com>
2026-02-13 19:50:32 +0900

    fix: Headerコンポーネントにリンクを追加し、レイアウトを調整

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,12 @@
 // import { useEffect, useState } from "react";
 import { Show } from "../responsive/show";
+import { Link } from "@/components/ui/link";
 
 
 const Header = () => {
 
     return (
-        <header className="m-5 p-3 rounded-lg border border-gray-200 mb-5 flex flex-col sm:flex-row items-center justify-between">
+        <header className="m-5 p-3 rounded-lg border border-gray-200 mb-5 flex flex sm:flex-row items-center justify-between">
             <h1 className="text-2xl font-bold">
                 <a href="/" className="p-0">tanahiro2010</a>
             </h1>
@@ -13,6 +14,13 @@ const Header = () => {
             <Show show="desktop" className="text-gray-600">
                 Scenario Writer / Software Developer
             </Show>
+
+
+            <Show show="desktop" className="text-gray-600 flex items-center space-x-2">
+                <Link href="https://github.com/tanahiro2010">GitHub</Link>
+                <Link href="https://twitter.com/tanahiro2010">Twitter</Link>
+            </Show>
+
         </header>
     );
 }
