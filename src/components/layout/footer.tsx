e8b211a04d42519ca86d73f411b2121d7f0d18c1
tanahiro2010 <herentongkegu087@gmail.com>
2026-03-12 20:59:07 +0900

    feat: フッターをスタイリングして中央揃えに変更

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,10 +2,10 @@
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className="text-center py-4 flex items-center justify-center flex-col space-y-2 border-t border-gray-200 mt-5">
             <p>© 2024 My Company. All rights reserved.</p>
         </footer>
-    )
+    );
 }
 
 export default Footer;
