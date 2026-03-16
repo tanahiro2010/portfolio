c18a235055578feac06bd19d30c0b7ee795278fa
tanahiro2010 <herentongkegu087@gmail.com>
2026-03-16 18:30:02 +0900

    feat: 外部リンクへのルーティングを追加

--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -1,8 +1,10 @@
 import type { HasChildrenRoute } from "@/types/routes";
-import { Navigate } from "react-router-dom";
+import { Navigate } from "@/components/utils/navigate";
 import About from "@/app/about";
+import Links from "@/app/links";
 
 const routes: Array<HasChildrenRoute> = [
+  /* サイト内ルーティング */
   {
     path: "/about", meta: { title: "About", navLabel: "About", visibleInNav: true },
     element: <About />,
@@ -13,17 +15,38 @@ const routes: Array<HasChildrenRoute> = [
     children: [],
   },
   {
-    path: "/blogs", meta: { title: "Blogs", navLabel: "Blogs", visibleInNav: true },
-    element: <Navigate to={`https://qiita.com/tanahiro2010`} />,
+    path: "/links", meta: { title: "Links", navLabel: "Links", visibleInNav: true },
+    element: <Links />
+  },
+  /* 外部サイトへのルーティング */
+  {
+    path: "/youtube", meta: { title: "YouTube", navLabel: "YouTube", visibleInNav: true },
+    element: <Navigate to={`https://www.youtube.com/@tanahiro2010`} />
   },
   {
-    path: "/links", meta: { title: "Links", navLabel: "Links", visibleInNav: true },
-    element: null
+    path: "/twitter", meta: { title: "Twitter", navLabel: "Twitter", visibleInNav: true },
+    element: <Navigate to={`https://twitter.com/tanahiro2010`} />
+  },
+  {
+    path: "/kakuyomu", meta: { title: "Kakuyomu", navLabel: "Kakuyomu", visibleInNav: true },
+    element: <Navigate to={`https://kakuyomu.jp/users/tanahiro2010`} />
+  },
+  {
+    path: "/github", meta: { title: "GitHub", navLabel: "GitHub", visibleInNav: true },
+    element: <Navigate to={`https://github.com/users/tanahiro2010`} />
+  },
+  {
+    path: "/zenn", meta: { title: "Zenn", navLabel: "Zenn", visibleInNav: true },
+    element: <Navigate to={`https://zenn.dev/tanahiro2010`} />
   },
   {
     path: "/contact", meta: { title: "Contact", navLabel: "Contact", visibleInNav: true },
     element: <Navigate to={`https://forms.gle/GmbZXQWXfwZpf4mr5`} />
-  }
+  },
+  {
+    path: "/blogs", meta: { title: "Blogs", navLabel: "Blogs", visibleInNav: true },
+    element: <Navigate to={`https://qiita.com/tanahiro2010`} />,
+  },
 ];
 
 export { routes };
