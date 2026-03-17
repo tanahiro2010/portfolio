93c57ec9eeb57706aa11a67e3707d97117c7b8a0
tanahiro2010 <herentongkegu087@gmail.com>
2026-03-17 14:59:30 +0900

    feat: リダイレクト中のメッセージとリンクを表示するコンポーネントを追加

--- src/components/utils/navigate.tsx
+++ src/components/utils/navigate.tsx
@@ -1,7 +1,23 @@
+import { Link } from "@/components/ui/link";
+
 const Navigate = ({ to }: { to: string }) => {
     window.location.href = to;
 
-    return null
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <h2 className="text-3xl font-bold mb-6 text-center">Redirecting...</h2>
+            <p className="text-center text-gray-600">
+                現在リダイレクト中です。 <br />
+                リダイレクトされない場合、下のリンクからページへお進みください。
+            </p>
+
+            <div className="text-center mt-6">
+                <Link href={to}>
+                    ページに進む
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export { Navigate };
